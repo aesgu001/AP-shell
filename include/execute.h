fa6b7df524ae8e7ff9cf2b206824fb55e3958870
Angelo Pahati <aesgu001@github.com>
2022-05-21 00:45:06 -0700

    Extend cd to handle HOME subdirectories

--- include/execute.h
+++ include/execute.h
@@ -1,13 +1,22 @@
 #ifndef __EXECUTE_H
 #define __EXECUTE_H
 
-#include "parse.h"
+#include "shell_consts.h"
 
-#include <stdio.h>      /*  perror                              */
-#include <stdlib.h>     /*  EXIT_FAILURE, EXIT_SUCCESS, exit    */
-#include <string.h>     /*  strcmp                              */
-#include <sys/wait.h>   /*  waitpid                             */
-#include <unistd.h>     /*  fork                                */
+#include <stdio.h>      /*  fprintf, perror, printf, stderr, stdout             */
+#include <stdlib.h>     /*  EXIT_FAILURE, EXIT_SUCCESS, exit, getenv, setenv    */
+#include <string.h>     /*  strcmp                                              */
+#include <sys/wait.h>   /*  waitpid                                             */
+#include <unistd.h>     /*  chdir, fork, getcwd                                 */
+
+/*
+*   Changes the current working directory to the directory specified in path.
+*
+*   @param  path    path to new directory.
+*   
+*   @return 0 on success, 1 on error.
+*/
+int executeChangeDirectory(const char *);
 
 /*
 *   Clones a child process to execute the command line argument, and retrieves that child process's
--- src/execute.c
+++ src/execute.c
@@ -1,5 +1,93 @@
 #include "../include/execute.h"
 
+int executeChangeDirectory(const char *path)
+{
+    /*  Path to previous working directory.     */
+    const char *oldpwd = NULL;
+    /*  Path to destination directory.          */
+    const char *destDir = NULL;
+    /*  Path to current working directory.      */
+    char pwd[_PATH_MAX];
+
+    /*  Get previous working directory  */
+    oldpwd = getenv(_ENV_PWD);
+    if (!oldpwd)
+    {
+        perror("getenv");
+        return 1;
+    }
+
+    if (!path || strcmp(path, "") == 0 || strstr(path, "~") == path)
+    {
+        /*  Get path to HOME directory  */
+        destDir = getenv(_ENV_HOME);
+        if (!destDir)
+        {
+            perror("getenv");
+            return 1;
+        }
+        else if (path)
+        {
+            /*  Get path to HOME subdirectory   */
+            if (strlen(destDir) + strlen(path + 1) >= _PATH_MAX)
+            {
+                fprintf(stderr, "cd: path length exceeds buffer size\n");
+                return 1;
+            }
+            /*  Store it in buffer  */
+            *pwd = '\0'; 
+            strcat(pwd, destDir);
+            strcat(pwd, path + 1);
+            destDir = pwd;
+        }
+    }
+    else if (strcmp(path, "-") == 0)
+    {
+        /*  Get path to OLDPWD directory    */
+        destDir = getenv(_ENV_OLDPWD);
+        if (!destDir)
+        {
+            perror("getenv");
+            return 1;
+        }
+        /*  Print it    */
+        printf("%s\n", destDir);
+    }
+    else
+    {
+        /*  Get path to destination directory   */
+        destDir = path;
+    }
+
+    /*  Change directory    */
+    if (chdir(destDir) < 0)
+    {
+        perror("chdir");
+        return 1;
+    }
+    /*  Set OLDPWD to previous env  */
+    else if (setenv(_ENV_OLDPWD, oldpwd, 1) < 0)
+    {
+        perror("setenv");
+        return 1;
+    }
+
+    /*  Get current working directory   */
+    else if (!getcwd(pwd, _PATH_MAX))
+    {
+        perror("getcwd");
+        return 1;
+    }
+    /*  Set PWD to new env  */
+    else if (setenv(_ENV_PWD, pwd, 1) < 0)
+    {
+        perror("setenv");
+        return 1;
+    }
+
+    return 0;
+}
+
 void executeCommand(char *const argv[], int *exitStatus)
 {
     /*  Child process status.   */
@@ -40,22 +128,29 @@ int executeInput(int argc, char *argv[], const char *toks[])
     /*  Command exit status.    */
     int exitStatus = 0;
     /*  Command connector.      */
-    const char *tok = _SEMICOLON;
+    const char *tok = _TOK_SC;
 
     int itArgv, itTok;
     for (itArgv = 0, itTok = 0; itArgv < argc && argv[itArgv];)
     {
         /*  Meet conditions before executing command    */
-        if (strcmp(tok, _SEMICOLON) == 0 || (strcmp(tok, _AND) == 0 && exitStatus == EXIT_SUCCESS) ||
-            (strcmp(tok, _OR) == 0 && exitStatus == EXIT_FAILURE))
+        if (strcmp(tok, _TOK_SC) == 0 || (strcmp(tok, _TOK_AND) == 0 && exitStatus == EXIT_SUCCESS) ||
+            (strcmp(tok, _TOK_OR) == 0 && exitStatus == EXIT_FAILURE))
         {
             /*  Exit program    */
-            if (strcmp(argv[itArgv], "exit") == 0)
+            if (strcmp(argv[itArgv], _CMD_EXIT) == 0)
             {
                 return 1;
             }
-
-            executeCommand(&argv[itArgv], &exitStatus);
+            /*  Change directory    */
+            else if (strcmp(argv[itArgv], _CMD_CD) == 0)
+            {
+                exitStatus = executeChangeDirectory(argv[itArgv + 1]);
+            }
+            else
+            {
+                executeCommand(&argv[itArgv], &exitStatus);
+            }
         }
 
         /*  Get next command    */
@@ -67,7 +162,7 @@ int executeInput(int argc, char *argv[], const char *toks[])
         }
         else
         {
-            tok = _SEMICOLON;
+            tok = _TOK_SC;
         }
     }
 
